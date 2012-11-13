5ee5fa86e7b5b58516e66dd8c2ee2d141d64e8d4
sanju9ece <sanju9ece@gmail.com>
2012-11-13 15:52:40 +0000

    From Sanju1

---
+++ src/org/sanju/newp/Sanju1.java
@@ -0,0 +1,13 @@
+package org.sanju.newp;
+
+public class Sanju1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sanju 1");
+	}
+
+}
