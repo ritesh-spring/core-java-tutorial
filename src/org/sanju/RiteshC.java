6083f7338d1b8c904c12d7ff3e76c57de63ce0bf
project.ejb <project.ejb@gmail.com>
2012-11-13 15:49:46 +0000

    From Ritesh

---
+++ src/org/sanju/RiteshC.java
@@ -0,0 +1,13 @@
+package org.sanju;
+
+public class RiteshC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi I am Ritesh");
+	}
+
+}
