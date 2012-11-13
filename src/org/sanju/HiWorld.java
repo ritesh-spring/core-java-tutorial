0c6d64fb244b595becda79656ac76ed2e3b8e553
sanju9ece <sanju9ece@gmail.com>
2012-11-13 14:32:15 +0000

    This First Program from Sanju

---
+++ src/org/sanju/HiWorld.java
@@ -0,0 +1,13 @@
+package org.sanju;
+
+public class HiWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi am Sanju");
+	}
+
+}
