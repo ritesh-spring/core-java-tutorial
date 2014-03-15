79525a299278410ffc2b66daee3a9d4c37623d75
aditya.kec20@gmail.com <aditya.kec20@gmail.com@00547fb4-64e5-4931-3b4a-561e02d6b7d4>
2014-03-15 18:38:27 +0000

    Commit

--- trunk/core-java-tutorial/ACoreJavaTutorial/src/org/aditya/program/Program1.java
+++ trunk/core-java-tutorial/ACoreJavaTutorial/src/org/aditya/program/Program1.java
@@ -5,7 +5,7 @@ package org.aditya.program;
 
 /**
  * @author aditya
- *
+ * 
  */
 public class Program1 {
 
@@ -14,7 +14,7 @@ public class Program1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Aditya WorkSpace");
 	}
 
 }
