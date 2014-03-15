4ca15a7330f4862dc11f6cbd2a0260a4ce570774
aditya.kec20 <aditya.kec20@gmail.com>
2014-03-15 18:36:07 +0000

    Commiting code from Aditya

---
+++ core-java-tutorial/ACoreJavaTutorial/src/org/aditya/program/Program1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.aditya.program;
+
+/**
+ * @author aditya
+ *
+ */
+public class Program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
